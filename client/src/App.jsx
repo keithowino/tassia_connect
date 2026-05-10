73c245a5115dad84811c5d4c0a53dd9515b1ae3f
Keith Owino <designsolutions1629@gmail.com>
2026-05-10 17:08:13 +0300

    Init commit.

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<section className="app">
+			<div className="container mx-auto flex flex-col items-center justify-center h-screen">
+				<h1 className="text-2xl italic font-bold">Tassia Connect</h1>
+				<p>Comming soon... </p>
+			</div>
+		</section>
+	);
+};
+
+export default App;
---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig(() => {
+	return {
+		server: {
+			port: 3000,
+			host: "0.0.0.0",
+		},
+		plugins: [react()],
+	};
+});
